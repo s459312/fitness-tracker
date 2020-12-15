4e344c1ab6927477f057c1aed97742fdf2dd5f50
adam-skowronek <adam.skowronek@grandmetric.com>
2020-12-15 23:51:52 +0100

    Add trainings

--- FitnessTracker/ClientApp/src/components/Trainings.js
+++ FitnessTracker/ClientApp/src/components/Trainings.js
@@ -8,10 +8,14 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import StarIcon from "@material-ui/icons/Star";
+import AddTraining from "./AddTraining";
+import Button from "@material-ui/core/Button";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -21,6 +25,16 @@ const useStyles = makeStyles((theme) => ({
   title: {
     margin: theme.spacing(4, 0, 2),
   },
+  headingWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  details: { display: "flex", flexDirection: "column" },
 }));
 
 const Trainings = () => {
@@ -29,23 +43,23 @@ const Trainings = () => {
   const [publicTrainings, setPublicTrainings] = useState([]);
   const [selectedTraining, setSelectedTraining] = useState();
 
+  const getUserTrainings = async () => {
+    try {
+      const { data } = await Api.get("/training");
+      setUserTrainings(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const getPublicTrainings = async () => {
+    try {
+      const { data } = await Api.get("/training/availablePublic");
+      setPublicTrainings(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   useEffect(() => {
-    const getUserTrainings = async () => {
-      try {
-        const { data } = await Api.get("/training");
-        setUserTrainings(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    const getPublicTrainings = async () => {
-      try {
-        const { data } = await Api.get("/training/availablePublic");
-        setPublicTrainings(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
     getUserTrainings();
     getPublicTrainings();
   }, []);
@@ -59,10 +73,46 @@ const Trainings = () => {
     }
   };
 
+  const toggleFavourite = async (id, favourite) => {
+    try {
+      await Api.patch("/training/toggleFavourite", {
+        trainingId: id,
+        favourite: !favourite,
+      });
+      getUserTrainings();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const addToHistory = async (id, exercises) => {
+    const exercisesList = exercises.map((exercise) => {
+      const stats = {
+        czas: exercise.czas ?? 0,
+        serie: exercise.serie ?? 0,
+        powtorzenia: exercise.powtorzenia ?? 0,
+        obciazenie: exercise.obciazenie ?? 0,
+        dystans: exercise.dystans ?? 0,
+      };
+      delete stats.goal;
+      delete stats.id;
+      delete stats.name;
+      return { exerciseId: exercise.id, exerciseHistoryStat: stats };
+    });
+    try {
+      await Api.patch("/training/addToHistory", {
+        trainingId: id,
+        exercises: exercisesList,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const TrainingsList = ({ trainings }) => (
     <div className={classes.demo}>
       <List dense={false}>
-        {trainings.length > 0 ? (
+        {trainings?.length > 0 ? (
           trainings.map((training) => (
             <ListItem
               button
@@ -72,9 +122,17 @@ const Trainings = () => {
             >
               <ListItemText primary={training.name} />
               <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="favorite">
-                  <StarBorderIcon />
-                </IconButton>
+                {training.isPublic === false && (
+                  <IconButton
+                    onClick={() =>
+                      toggleFavourite(training.id, training.favourite)
+                    }
+                    edge="end"
+                    aria-label="favorite"
+                  >
+                    {training.favourite ? <StarIcon /> : <StarBorderIcon />}
+                  </IconButton>
+                )}
               </ListItemSecondaryAction>
             </ListItem>
           ))
@@ -85,20 +143,20 @@ const Trainings = () => {
     </div>
   );
 
+  const excersiseParagraph = (title, field) =>
+    field && field > 0 ? (
+      <Typography paragraph>
+        {title}: {field}
+      </Typography>
+    ) : null;
+
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} md={4} lg={3}>
         <Typography align="center" gutterBottom variant="h5">
           Moje plany treningowe
         </Typography>
-        <Button
-          variant="contained"
-          style={{ width: "100%" }}
-          color="primary"
-          size="large"
-        >
-          Dodaj nowy plan
-        </Button>
+        <AddTraining callback={getUserTrainings} />
         <TrainingsList trainings={userTrainings} />
         <Typography align="center" gutterBottom variant="h5">
           Gotowe plany treningowe
@@ -106,9 +164,58 @@ const Trainings = () => {
         <TrainingsList trainings={publicTrainings} />
       </Grid>
       <Grid item xs={12} md={8} lg={9}>
+        {selectedTraining && (
+          <>
+            <Typography gutterBottom variant="h5">
+              {selectedTraining?.name}
+              <Button
+                variant="contained"
+                style={{ marginLeft: 20 }}
+                color="primary"
+                size="large"
+                onClick={() =>
+                  addToHistory(
+                    selectedTraining.id,
+                    selectedTraining.trainingExercises
+                  )
+                }
+              >
+                Dodaj do historii
+              </Button>
+            </Typography>
+            {selectedTraining.description && (
+              <Typography gutterBottom variant="h6">
+                Opis
+              </Typography>
+            )}
+            <p>{selectedTraining?.description}</p>
+            {selectedTraining.trainingExercises.length > 0 && (
+              <Typography gutterBottom variant="h6">
+                Ćwiczenia
+              </Typography>
+            )}
+          </>
+        )}
         {selectedTraining ? (
           selectedTraining?.trainingExercises.map((exercise) => (
-            <p>{exercise.name}</p>
+            <Accordion key={exercise.id}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <div>
+                  <Typography className={classes.heading}>
+                    {exercise.name}
+                  </Typography>
+                </div>
+              </AccordionSummary>
+              <AccordionDetails className={classes.details}>
+                <Typography paragraph>Cel: {exercise.goal}</Typography>
+                {excersiseParagraph("Opis", exercise.description)}
+                {excersiseParagraph("Serie", exercise.serie)}
+                {excersiseParagraph("Powtórzenia", exercise.powtorzenia)}
+                {excersiseParagraph("Czas", exercise.czas)}
+                {excersiseParagraph("Obciążenie", exercise.obciazenie)}
+                {excersiseParagraph("Dystans", exercise.dystans)}
+              </AccordionDetails>
+            </Accordion>
           ))
         ) : (
           <Typography variant="h5">Wybierz plan</Typography>
